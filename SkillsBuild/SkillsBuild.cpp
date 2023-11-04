3f7ca6d0243ea677e6a71db5ced6fbb52732afd5
anji <anji_cruz@msn.com>
2023-11-04 22:45:46 +0800

    Add project files.

---
+++ SkillsBuild/SkillsBuild.cpp
@@ -0,0 +1,13 @@
+// SkillsBuild.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <stdio.h>
+using namespace std;
+
+int main()
+{
+    cout << "Hello World!\n";
+}
+
+
